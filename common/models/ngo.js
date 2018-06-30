5b476aa82543930a96e538cce42cb6eb9c598b67
Gokul Kulkarni <gokul.cs5@gmail.com>
2018-06-30 14:17:45 +0530

    inserting correct scope ID

--- common/models/ngo.js
+++ common/models/ngo.js
@@ -8,11 +8,10 @@ module.exports = function(Ngo) {
     console.log('app module >>>>>>>>>>>>> ', app);
     var user_role = app.models.user_role;
     user_role.create({
-      scopeId: 'NGO',
-      ngoUserId: remoteMethodOutput.Id,
+      scopeId: remoteMethodOutput.id,
+      ngoUserId: remoteMethodOutput.ngoUserId,
       roleId: '5b3626e1f12e5373953103ea',
     });
-    //context.models.
     next();
   });
 };
