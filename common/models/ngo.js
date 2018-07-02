89168c8d470005265ed9996483b333823d34fa02
Gokul Kulkarni <gokul.cs5@gmail.com>
2018-07-02 14:04:50 +0530

    code refactoring

--- common/models/ngo.js
+++ common/models/ngo.js
@@ -2,10 +2,9 @@
 
 var app = require('../../server/server');
 module.exports = function(Ngo) {
-
   Ngo.afterRemote('create', (context, remoteMethodOutput, next) => {
-    var user_role = app.models.user_role;
-    user_role.create({
+    var userRole = app.models.user_role;
+    userRole.create({
       scopeId: remoteMethodOutput.id,
       ngoUserId: remoteMethodOutput.ngoUserId,
       roleId: '5b3626e1f12e5373953103ea',
